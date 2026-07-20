f5cab2164b5314574b913bc4402147d9c554b0d2
antnish1 <shahdolho@gmail.com>
2026-07-20 22:51:55 +0530

    Refresh cache for expense ledger fixes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mainstreet-business-v11";
+const CACHE_NAME = "mainstreet-business-v12";
 const APP_SHELL = ["./","./index.html","./styles.css","./category-photos.css","./category-images-individual.css","./business-enhancements.css","./category-flow.js","./business-enhancements.js","./dashboard-item-search.js","./account-summary.js","./app.js","./menu-data.js","./supabase-api.js","./supabase-config.js","./manifest.webmanifest","./assets/mainstreet-logo.svg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting()});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim()});
